f61072e3eb602d4bbaf0a9ec9bad5af3d93c4dec
Edi <poqueque@yahoo.es>
2024-01-09 12:39:52 +0100

    After Session 8 - Internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:chatter/firebase_options.dart';
 import 'package:chatter/providers/message_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 import 'screens/splash.dart';
@@ -22,6 +24,17 @@ class MyApp extends StatelessWidget {
       create: (BuildContext context) => MessageProvider(),
       child: MaterialApp(
         title: 'Flutter Demo',
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          FirebaseUILocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('es'),
+          Locale('ca'),
+          Locale('en'),
+        ],
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
